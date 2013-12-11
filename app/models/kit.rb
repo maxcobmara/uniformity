9964fa2912a8b5d16e971d9e9d17ffe6bd64a33b
Nurhashimah <Shimah@MacBook-2.local>
2013-12-12 01:37:46 +0800

    Relationship & details for select field-kit,uniform_item & kit_uniform and Added field for kit_uniform (senior_rate)

--- app/models/kit.rb
+++ app/models/kit.rb
@@ -1,2 +1,6 @@
 class Kit < ActiveRecord::Base
+  has_many :kit_uniforms
+  def kit_details
+    "#{combo_code} | #{name}"
+  end
 end
--- app/models/uniform_item.rb
+++ app/models/uniform_item.rb
@@ -1,2 +1,6 @@
 class UniformItem < ActiveRecord::Base
+  has_many :kit_uniforms
+  def uniform_details
+    "#{code} | #{name}" 
+  end
 end
