1f9f78dd2574ab82e3b72bcda580c2a9167c4735
nurhashimah <nurhashimah77@gmail.com>
2014-04-06 12:41:42 +0800

    RV No & Inv No added into Uniform Stock Received & Uniform Stock Issued

--- app/models/uniform_stock_issue.rb
+++ app/models/uniform_stock_issue.rb
@@ -1,8 +1,25 @@
 class UniformStockIssue < ActiveRecord::Base
+  before_save :save_my_vars
   belongs_to :uniform_stock, :foreign_key => 'stock_id'
   belongs_to :issuer, :class_name => 'Staff', :foreign_key => 'issued_by'
   belongs_to :receiver, :class_name => 'Staff', :foreign_key => 'issued_to'
   validates_length_of :quantity,  :maximum => 5,
                                   :message => "maximum is 5 characters"
-  validates       :stock_id, presence: true
+  validates :stock_id, :issued_on, presence: true
+  
+ def save_my_vars
+	latest_ivno = UniformStockIssue.last.iv_no
+	if id == nil
+		if latest_ivno.nil?
+			self.iv_no = format_iv+"-1"
+		else
+			a=(latest_ivno.split("-")[1]).to_i+1
+			self.iv_no = format_iv+"-"+a.to_s
+		end
+	end
+  end
+  
+  def format_iv
+	"IV"+issued_on.strftime("%Y%m")
+  end
 end
--- app/models/uniform_stock_received.rb
+++ app/models/uniform_stock_received.rb
@@ -1,4 +1,5 @@
 class UniformStockReceived < ActiveRecord::Base
+  before_save :save_my_vars
   belongs_to :uniform_stock, :foreign_key => 'stock_id'
   belongs_to :receiver, :class_name => 'Staff', :foreign_key => 'received_by'
   belongs_to :contract, :foreign_key => 'contract_id'
@@ -6,5 +7,21 @@ class UniformStockReceived < ActiveRecord::Base
                                   :message => "maximum is 5 characters"
   validates_length_of :size,      :maximum => 15,
                                   :message => "maximum is 15 characters"
-  validates       :stock_id, presence: true
+  validates :stock_id, :received_on, presence: true
+  
+  def save_my_vars
+	latest_rvno = UniformStockReceived.last.rv_no
+	if id == nil
+		if latest_rvno.nil?
+			self.rv_no = format_rv+"-1"
+		else
+			a=(latest_rvno.split("-")[1]).to_i+1
+			self.rv_no = format_rv+"-"+a.to_s
+		end
+	end
+  end
+  
+  def format_rv
+	"RV"+received_on.strftime("%Y%m")
+  end
 end
