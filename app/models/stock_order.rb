926adacc1efdb4167e176df4b3bceb864e81ab55
Nurhashimah <nurhashimah77@gmail.com>
2014-04-19 02:55:29 +0800

    Unifor Stock Order-delete(show) route error fixed, validation added & fixes for new/create, index, show

--- app/models/stock_order.rb
+++ app/models/stock_order.rb
@@ -2,4 +2,5 @@ class StockOrder < ActiveRecord::Base
   belongs_to :uniform_stock, :foreign_key => "stock_id"
   belongs_to :unit_type, :foreign_key => "unit_type_id"
   belongs_to :supplier, :class_name => "Company", :foreign_key => "company_id"
+  validates_presence_of :stock_id
 end
